3b116585c1a28dc1b4763ecae8b425287cefa021
angelo gino varrati <angelo.gino.varrati@avanade.com>
2024-01-29 19:04:23 +0100

    add current client info

---
+++ Data/CurrentClient/outputs.tf
@@ -0,0 +1,19 @@
+output "client_id" {
+  value = data.azurerm_client_config.current.client_id
+  sensitive = true
+}
+
+output "object_id" {
+  value = data.azurerm_client_config.current.client_id
+  sensitive = true
+}
+
+output "tenant_id" {
+  value = data.azurerm_client_config.current.tenant_id
+  sensitive = true
+}
+
+output "subscription_id" {
+  value = data.azurerm_client_config.current.subscription_id
+  sensitive = true
+}
