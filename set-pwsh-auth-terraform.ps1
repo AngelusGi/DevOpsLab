1c88cd62e53a82fc760b960c9254437b9518a737
angelo gino varrati <angelo.gino.varrati@avanade.com>
2024-01-31 19:52:50 +0100

    rollback

--- set-pwsh-auth-terraform.ps1
+++ set-pwsh-auth-terraform.ps1
@@ -1,13 +1,5 @@
 # # # PowerShell
-Set-Variable -Name '$env:ARM_CLIENT_ID' -Value "00000000-0000-0000-0000-000000000000"
-Set-Variable -Name '$env:ARM_CLIENT_SECRET' -Value "12345678-0000-0000-0000-000000000000"
-Set-Variable -Name '$env:ARM_TENANT_ID' -Value "10000000-0000-0000-0000-000000000000"
-Set-Variable -Name '$env:ARM_SUBSCRIPTION_ID' -Value "20000000-0000-0000-0000-000000000000"
-
-$varList = @('$env:ARM_CLIENT_ID', '$env:ARM_CLIENT_SECRET', '$env:ARM_TENANT_ID', '$env:ARM_SUBSCRIPTION_ID')
-
-foreach ($envVar in $varList) {
-    if ( [string]::IsNullOrEmpty((Get-Variable -Name $envVar -ValueOnly -ErrorAction SilentlyContinue))) {
-        Write-Error "Variable $($envVar) not set."
-    }
-}
+$env:ARM_CLIENT_ID = "00000000-0000-0000-0000-000000000000"
+$env:ARM_CLIENT_SECRET = "12345678-0000-0000-0000-000000000000"
+$env:ARM_TENANT_ID = "10000000-0000-0000-0000-000000000000"
+$env:ARM_SUBSCRIPTION_ID = "20000000-0000-0000-0000-000000000000"
