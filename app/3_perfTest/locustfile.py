d271f01d98c0c39026343041ced4ee3764cc6f38
angelusgi <angelovarrati@hotmail.it>
2024-02-06 23:16:48 +0100

    add simple perf test of api using locust

---
+++ app/3_perfTest/locustfile.py
@@ -0,0 +1,15 @@
+import time
+from locust import HttpUser, task, between
+
+class QuickstartUser(HttpUser):
+    # seconds between requests
+    wait_time = between(1, 5)
+
+    @task
+    def get_randoms(self):
+        for item_id in range(10):
+            api = "/getRandom"
+            response = self.client.get(api)
+            print("Response status code:", response.status_code)
+            print("Response text:", response.text)
+            time.sleep(1)
